2be98ba3f7105bbb5927a9a74806de73e8caaaa7
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-02-04 11:41:05 +0200

    update helpers

--- helpers.go
+++ helpers.go
@@ -1,6 +1,7 @@
 package mobiledoc
 
 func contains(list []string, str string) bool {
+	// check existence
 	for _, item := range list {
 		if item == str {
 			return true
@@ -11,11 +12,19 @@ func contains(list []string, str string) bool {
 }
 
 func toInt(v interface{}) (int, bool) {
+	// check int
 	i, ok := v.(int)
 	if ok {
 		return i, true
 	}
 
+	// check int64 (bson)
+	ii, ok := v.(int64)
+	if ok {
+		return int(ii), true
+	}
+
+	// check float64 (json)
 	f, ok := v.(float64)
 	if ok {
 		return int(f), true
